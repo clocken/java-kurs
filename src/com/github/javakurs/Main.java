0e98407abf33c9c1ed1c856a4fb67deb1ebdadf4
PDruschl <157515224+PDruschl@users.noreply.github.com>
2024-01-26 16:32:03 +0100

    Update Übung 1
    Update der ersten Übung mit Zahlentypen als Variablen.
    
    Co-Authored-By: clocken <66382986+clocken@users.noreply.github.com>

--- src/com/github/javakurs/Main.java
+++ src/com/github/javakurs/Main.java
@@ -11,5 +11,23 @@ public class Main { //Deklaration der Klasse
 
     public static void main(String[] args) { //Deklaration der Methode(Methodensignatur)
         System.out.println("Hello World"); //Was die Methode macht
+
+        String x; //Variablendeklaration(Typ=String "x"(Name))
+        x = "Hello World"; //Wertzuweisung der Var. (Initialisierung)
+        System.out.println(x);
+
+        String hello = "Hello";
+        String world = "World";
+        String helloWorld = hello + " " + world;
+        System.out.println(helloWorld);
+
+        int wert0 = 0;
+        System.out.println(wert0);
+        String helloWorldLeetSpeak = "hell" + wert0 + "W" + wert0 + "rldLeetSpeak";
+        System.out.println(helloWorldLeetSpeak);
+
+        int add1 = 2;
+        int add2 = 5;
+        System.out.println(add1 + add2);
     }
 }
